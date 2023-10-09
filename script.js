c29695c2ccb378b0b1c7c3e1715843b3efc40866
Sk. Imran Hussain <skimranpc@gmail.com>
2023-10-09 21:59:28 +0530

    initGame, checkWinner, draw, many more logics added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const playerInfo = document.querySelector('.player-info')
 const boxes = document.querySelectorAll('.box')
+const newGameButton = document.querySelector('.new-game')
 
 let currentPlayer = 'X'
+let fillCount = 0;
 let boxStatus = ["","","","","","","","",""]
 let winningPattern = [
 ['0','1','2'],
@@ -19,7 +21,16 @@ console.log(winningPattern);
 initGame()
 
 function initGame(){
+    currentPlayer = 'X'
     playerInfo.innerText = `Current Player - ${currentPlayer}`
+    boxStatus = ["","","","","","","","",""]
+    newGameButton.classList.remove("active");
+
+    boxes.forEach((box) => {
+        box.innerText = "";
+        box.style.pointerEvents = "all";
+    })
+
 }
 
 function switchCurrentPlayer(){
@@ -28,15 +39,54 @@ function switchCurrentPlayer(){
     } else {
         currentPlayer = 'X'
     }
+
+    playerInfo.innerText = `Current Player - ${currentPlayer}`
+}
+
+function checkWinner(){
+    let answer = "";
+
+    winningPattern.forEach((position) => {
+        if (boxStatus[position[0]] != "" && boxStatus[position[1]] != "" && boxStatus[position[2]] != "" &&
+        boxStatus[position[0]] === boxStatus[position[1]] && boxStatus[position[1]] === boxStatus[position[2]]) {
+            console.log("winner");
+            answer = boxStatus[position[0]]
+            playerInfo.innerText = `Winner Player - ${answer}`
+            // currently writing here
+            
+            gameOver()
+        }
+
+        function gameOver(){
+            boxes.forEach((box) => {
+                box.style.pointerEvents = "none";
+                newGameButton.classList.add("active");
+            })
+        }
+
+        if (answer === "" && fillCount === 9) {
+            playerInfo.innerText = `It's a Draw`
+            gameOver()
+        }
+
+        console.log(fillCount);
+
+
+
+    })
 }
 
 boxes.forEach((box, index) => {
     box.addEventListener('click', () => {
         box.innerText = `${currentPlayer}`
         boxStatus[index] = `${currentPlayer}`
+        box.style.pointerEvents = "none";
+        fillCount++;
         switchCurrentPlayer();
         checkWinner()
         console.log(boxStatus);
 
     })
 })
+
+newGameButton.addEventListener('click', initGame);
